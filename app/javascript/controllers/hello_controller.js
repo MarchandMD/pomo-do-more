15d65e6730a692295f8bd2c3c8b0d019e0d84d79
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2024-04-22 08:30:59 -0600

    starts work on bonus time timer

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,8 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["number", "button", "playButton", "pauseButton", "audio"];
+  static targets = ["number", "button", "playButton", "pauseButton", "audio", "trackTitle", "bonusTime", "bonusTimeTimer"];
   static values = { tracks: String };
+  static outlets = ["stats"];
 
   connect() {
     this.numberTarget.innerText = 1500;
@@ -24,6 +25,7 @@ export default class extends Controller {
       this.buttonTarget.innerText = "start";
       this.numberTarget.innerText = 1500;
       this.pauseMusic();
+      this.bonusTimeTarget.classList.add("d-none");
     } else {
       this.buttonTarget.innerText = "start"
       this.stopTimer();
@@ -39,12 +41,21 @@ export default class extends Controller {
         this.stopTimer();
         this.pauseMusic();
         this.buttonTarget.innerText = "reset";
+        this.bonusTimeTarget.classList.remove("d-none");
+        this.startBonusTimeTimer();
       }
     }, interval);
   }
 
   stopTimer() {
     clearInterval(this.timer);
+  }
+
+  startBonusTimeTimer() {
+    const interval = 1000;
+    this.timer = setInterval(() => {
+      this.bonusTimeTimerTarget.innerText++;
+    }, interval)
   }
 
   playMusic() {
@@ -58,9 +69,27 @@ export default class extends Controller {
   playNextTrack() {
     const tracks = this.tracksValue.split(",");
     let randomIndex = Math.floor(Math.random() * tracks.length);
+    let nextTrack = tracks[randomIndex];
 
-    this.audioTarget.src = tracks[randomIndex];
+    this.audioTarget.src = nextTrack;
     this.audioTarget.play();
-    document.title = `new song: ${randomIndex}`;
+    let newTitle = nextTrack.match(/_(\d{1,2}.*?)\./)[1];
+    this.setNewTitle(newTitle);
   }
+
+  setNewTitle(title) {
+    document.title = title;
+    this.trackTitleTarget.innerText = title;
+  }
+
+  sessionEnded() {
+    // accumulate time
+    //
+    // start bonus timer
+    // bonus timer appears
+    // bonus timer begins counting up
+    // bonus timer has a start and stop button
+    // when the bonus stop button is clicked, accumulate and display the total
+  }
+
 }
